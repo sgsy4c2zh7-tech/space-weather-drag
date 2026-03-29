9c7732dbf0ebc781ac21caecd6ccc5b3893bb22d
sgsy4c2zh7-tech <sgsy4c2zh7@privaterelay.appleid.com>
2026-03-29 15:16:19 +0900

    Update fetch_tle.py

--- scripts/fetch_tle.py
+++ scripts/fetch_tle.py
@@ -1,1 +1,7 @@
+import requests
 
+url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle"
+res = requests.get(url)
+
+with open("data/latest.tle", "w") as f:
+    f.write(res.text)
