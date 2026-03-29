871fb121b3b770460483d1256b2aca6549d15ba4
sgsy4c2zh7-tech <sgsy4c2zh7@privaterelay.appleid.com>
2026-03-29 15:18:55 +0900

    Add script to generate satellite history JSON

--- scripts/build_history.py
+++ scripts/build_history.py
@@ -1,1 +1,18 @@
+import json
+from datetime import datetime
 
+data = {
+  "norad_id": 25544,
+  "name": "ISS",
+  "history": [
+    {
+      "epoch": datetime.utcnow().isoformat(),
+      "apogee_km": 420,
+      "perigee_km": 415,
+      "inclination_deg": 51.6
+    }
+  ]
+}
+
+with open("docs/data/25544.json", "w") as f:
+    json.dump(data, f, indent=2)
